580763b8d2cbcea03588c6c56d232790a924c6d0
Siddam Vamsee <31583305+ssvamsee@users.noreply.github.com>
2024-04-18 17:19:27 +0530

    Add files via upload

---
+++ src/components/services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title" data-aos="fade-down">
+          <h2>Our Services</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className="row" data-aos="fade-up">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4" data-aos="flip-right">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
